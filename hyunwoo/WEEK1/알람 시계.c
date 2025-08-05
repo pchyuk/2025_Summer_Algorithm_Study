fbf4cb4480f32f22253323f658688e32d771d16d
kimyeonu1 <hwoo1723@gmail.com>
2025-08-05 15:46:03 +0900

    Update 알람 시계.c

--- hyunwoo/WEEK1/알람 시계.c
+++ hyunwoo/WEEK1/알람 시계.c
@@ -6,13 +6,13 @@ int main() {
 
     scanf("%d %d", &hour, &min);
 
-    min = min - 45; 
+    min = min - 45; // 원래 설정된 알림시간 보다 45분 일찍 앞서는 시간 설정
     
     if (min < 0) {
-        min = min + 60;
-        hour = hour - 1;
+        min = min + 60; // 입력된 시간(분)에서 45분을 뺐을때 음수값이라면 60분을 더하고
+        hour = hour - 1; // 한 시간을 뺀다
         if (hour < 0) {
-            hour = hour + 24;
+            hour = hour + 24; // 또한 0시인경우 1을 빼고 24시간을 더한다
         }
     }
 
