e88d2d52c2c5632dc50e2e3e1f33c1b9feb0ddf1
kimyeonu1 <hwoo1723@gmail.com>
2025-08-21 14:44:03 +0900

    Update 최대공약수와 최소공배수.c

--- hyunwoo/WEEK4/최대공약수와 최소공배수.c
+++ hyunwoo/WEEK4/최대공약수와 최소공배수.c
@@ -1,1 +1,42 @@
 
+#include<stdio.h>
+
+int transform(int m, int n) {
+	int temp;
+	temp = m;
+	m = n;
+	n = temp;
+	return 0;
+}
+
+int gcb(int a, int b) {
+
+	while (b != 0) {
+		if (a < b) transform(a, b);
+		int temp = a % b;
+		a = b;
+		b = temp;
+	}
+	return a;
+}
+
+int lcb(int c, int d) {
+	if (c < d) transform(c, d);
+	return (c * d) / gcb(c, d);
+}
+
+int main() {
+
+	int x, y;
+
+	scanf("%d %d", &x, &y);
+
+	int result1 = gcb(x, y);
+	int result2 = lcb(x, y);
+
+	printf("%d\n", result1);
+	printf("%d", result2);
+
+	return 0;
+
+}
