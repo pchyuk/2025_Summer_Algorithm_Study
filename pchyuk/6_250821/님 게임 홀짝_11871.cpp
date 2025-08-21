4b5243e2c90b8a42e24bcf697bd49891a861b37d
park chanhyuk <pch6590@gmail.com>
2025-08-21 17:34:59 +0900

    6일차 문제 2문제 추가 commit

--- pchyuk/6_250821/님 게임 홀짝_11871.cpp
+++ pchyuk/6_250821/님 게임 홀짝_11871.cpp
@@ -11,7 +11,7 @@ int main(){
         else result ^= (p+1)/2;
     }
 
-    if(result = 0) cout << "cubelover\n";
+    if(result == 0) cout << "cubelover\n";
     else cout << "koosaga\n";
 
     return 0;
