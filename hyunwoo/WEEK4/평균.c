54d4fb7903f36bc6cb08e9a0ae29c285fd61dc28
kimyeonu1 <hwoo1723@gmail.com>
2025-08-26 19:05:16 +0900

    Update 평균.c

--- hyunwoo/WEEK4/평균.c
+++ hyunwoo/WEEK4/평균.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    
+    int a;
+    int score[1000];
+    double sum = 0;
+    double max = 0;
+
+    scanf("%d", &a);
+    
+    for (int i = 0;i < a;i++) {
+        scanf("%d", &score[i]);
+        if (score[i] > max) max = score[i];
+    }
+
+    for (int i = 0;i < a;i++) {
+        sum += (score[i] / max) * 100;
+    }
+
+    printf("%lf", sum / a);
+
+    return 0;
+}
