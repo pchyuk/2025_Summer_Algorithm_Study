381eed8a8ad4398098b870a62abf6c6e697e6b48
kimyeonu1 <hwoo1723@gmail.com>
2025-08-28 14:42:54 +0900

    Update 웰컴키트.c

--- hyunwoo/WEEK3/웰컴키트.c
+++ hyunwoo/WEEK3/웰컴키트.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    
+    int num;
+    int tsize[6];
+    int t, p;
+    int tshirt = 0;
+
+    scanf("%d", &num);
+
+    for (int i = 0;i < 6;i++) {
+        scanf("%d ", &tsize[i]);
+    }
+
+    scanf("%d %d", &t, &p);
+
+    for (int i = 0;i < 6;i++) {
+        if (tsize[i] > 0) {
+            tshirt += (tsize[i] + t - 1) / t;
+        }
+    }
+
+    int pen = num / p;
+    int leftpen = num % p;
+
+    printf("%d\n", tshirt);
+    printf("%d %d", pen, leftpen);
+
+
+    return 0;
+}
